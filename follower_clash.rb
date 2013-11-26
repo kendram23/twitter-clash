733ad07c1c01614d4982326d636124c96b72a9ea
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-11-26 16:49:42 -0600

    Added a following method as well as reconfigured the client = Twitter::REST so that it is in the initialize method.

--- follower_clash.rb
+++ follower_clash.rb
@@ -1,6 +1,6 @@
-require_relative 'lib/user_comparer'
+require_relative 'lib/user_compare.rb'
 
-require_relative 'lib/user_comparer'
+require_relative 'lib/user_compare.rb'
 
 andy = UserComparer::User.new('AusinAus')
 puts andy
@@ -8,21 +8,24 @@ puts andy
 kendra = UserComparer::User.new('kendram23')
 puts kendra
 
-tejas = UserComparer::User.new('tejasrawal90')
-puts tejas
+# tejas = UserComparer::User.new('tejasrawal90')
+# puts tejas
 
-matt = UserComparer::User.new('LarshMatt')
-puts matt
+# matt = UserComparer::User.new('LarshMatt')
+# puts matt
 
-don = UserComparer::User.new('DonDrums')
-puts don
+# don = UserComparer::User.new('DonDrums')
+# puts don
 
 puts "#{andy.username}'s follower count: #{andy.followers}"
 puts "#{kendra.username}'s follower count: #{kendra.followers}"
-puts "#{tejas.username}'s follower count: #{tejas.followers}"
-puts "#{matt.username}'s follower count: #{matt.followers}"
-puts "#{don.username}'s follower count: #{don.followers}"
+# puts "#{tejas.username}'s follower count: #{tejas.followers}"
+# puts "#{matt.username}'s follower count: #{matt.followers}"
+# puts "#{don.username}'s follower count: #{don.followers}"
+puts "#{andy.username}'s follower count: #{andy.following}"
+puts "#{kendra.username}'s follower count: #{kendra.following}"
 
-
-new_comparison = UserComparer::Comparer.new(andy, kendra, tejas, matt, don)
-new_comparison.compare(andy, kendra, tejas, matt, don)
+new_comparison = UserComparer::Comparer.new(andy, kendra)
+new_comparison.compare_followers(andy, kendra)
+new_comparison.compare_following(andy, kendra)
+new_comparison.follow_ratio(andy, kendra)
--- lib/user_compare.rb
+++ lib/user_compare.rb
@@ -8,38 +8,56 @@ module UserComparer
 
 		def initialize(username)
 			@username = username
+			@client = Twitter::REST::Client.new do |config|
+				config.consumer_key 	   = ENV['CONSUMER_KEY']
+				config.consumer_secret     = ENV['CONSUMER_SECRET']
+				config.access_token		   = ENV['OAUTH_TOKEN']
+				config.access_token_secret = ENV['OAUTH_TOKEN_SECRET']
+		end
+
 		end
 
 		def followers
-			client = Twitter::REST::Client.new do |config|
-				config.consumer_key 		    = ENV['CONSUMER_KEY']
-				config.consumer_secret     = ENV['CONSUMER_SECRET']
-				config.access_token			    = ENV['OAUTH_TOKEN']
-				config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
-			end
+			@client.user(@username).followers_count
+		end
 
-			client.user(@username).followers_count
+		def following
+			@client.user(@username).friends_count
 		end
 	end
 
 	class Comparer
 		
-		def initialize(user1, user2, user3, user4, user5)
+		def initialize(user1, user2)
 			@user1 = user1
 			@user2 = user2
-			@user3 = user3
-			@user4 = user4
-			@user5 = user5
 		end
 
-		def compare(user1, user2, user3, user4, user5)
+		def compare_followers(user1, user2)
 			if user1.followers > user2.followers
-				puts user1.username
+				puts "#{user2.username} has more followers"
 			elsif user1.followers < user2.followers
-				puts user2.username
+				puts "#{user2.username} has more followers"
 			else
 				puts "Users have the same number of followers"
 			end
+		end
+
+		def compare_following(user1, user2)
+			if user1.following > user2.following
+				puts "#{user1.username} is following more users"
+			elsif user1.following < user2.following
+				puts "#{user2.username} is following more users"
+			else
+				puts "Users are following the same number of people"
+			end
+		end
+
+		def follow_ratio(user1, user2)
+			user1_ratio = (user1.followers/user2.following) * 100
+			puts "#{user1.username} has a ratio of #{user1_ratio} followers to following"
+			user2_ratio = user2.followers/user2.following
+			puts "#{user2.username} has a ratio of #{user2_ratio} followers to following"
 		end
 	end
 end
